fbc81433c92e84908bdab2a3fbe762bbc9287669
Devin Power <dpwr16@gmail.com>
2023-07-04 19:25:12 +0000

    add surrogate key to clean_items

--- backend/dbt/mndot_bid/models/clean/clean_items.sql
+++ backend/dbt/mndot_bid/models/clean/clean_items.sql
@@ -6,4 +6,15 @@ with source as (
 
 )
 
-select * from source
+select 
+    {{ dbt_utils.generate_surrogate_key(
+        [
+            's.spec_code', 
+            's.unit_code', 
+            's.item_code', 
+            's.long_description', 
+            's.unit_name'
+        ]) 
+    }} as id,
+    s.* 
+from source s
--- backend/dbt/mndot_bid/models/interim/int_spec_year_matrix.sql
+++ backend/dbt/mndot_bid/models/interim/int_spec_year_matrix.sql
@@ -1,56 +1,24 @@
 with items as (
-    select 
-        spec_code, 
-        unit_code, 
-        item_code,
-        short_description,
-        long_description, 
-        unit_name,
-        plan_unit_description
-    from 
-        {{ ref('clean_items') }}
+    select * exclude(spec_year)
+    from {{ ref('clean_items') }}
 ),
 
 spec_2016 as (
-    select 
-        spec_code, 
-        unit_code, 
-        item_code, 
-        long_description, 
-        unit_name,
-        true as in_spec_2016 
-    from 
-        {{ ref('clean_items') }}
-    where 
-        spec_year = 2016
+    select id, true as in_spec_2016 
+    from {{ ref('clean_items') }}
+    where spec_year = 2016
 ),
 
 spec_2018 as (
-    select 
-        spec_code, 
-        unit_code, 
-        item_code, 
-        long_description, 
-        unit_name,
-        true as in_spec_2018  
-    from 
-        {{ ref('clean_items') }}
-    where 
-        spec_year = 2018
+    select id, true as in_spec_2018  
+    from {{ ref('clean_items') }}
+    where spec_year = 2018
 ),
 
 spec_2020 as (
-    select 
-        spec_code, 
-        unit_code, 
-        item_code, 
-        long_description, 
-        unit_name,
-        true as in_spec_2020 
-    from 
-        {{ ref('clean_items') }}
-    where 
-        spec_year = 2020
+    select id, true as in_spec_2020 
+    from {{ ref('clean_items') }}
+    where spec_year = 2020
 ),
 
 joined as (
@@ -59,39 +27,15 @@ joined as (
         ifnull(spec_2016.in_spec_2016, false) as in_spec_2016,
         ifnull(spec_2018.in_spec_2018, false) as in_spec_2018,
         ifnull(spec_2020.in_spec_2020, false) as in_spec_2020,
-    from
-        items
-    left join 
-        spec_2016 
-    using (
-        spec_code, 
-        unit_code, 
-        item_code, 
-        long_description, 
-        unit_name,
-    )
-    left join
-        spec_2018
-    using (
-        spec_code, 
-        unit_code, 
-        item_code, 
-        long_description, 
-        unit_name,
-    )
-    left join
-        spec_2020
-    using (
-        spec_code, 
-        unit_code, 
-        item_code, 
-        long_description, 
-        unit_name,
-    )
+    from items
+    left join spec_2016 using ( id )
+    left join spec_2018 using ( id )
+    left join spec_2020 using ( id )
 ),
 
 item_matrix as (
-    select distinct * from joined
+    select distinct on(id) *
+    from joined
 )
 
 select * from item_matrix
