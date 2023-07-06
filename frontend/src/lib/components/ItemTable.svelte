132757d21b1a55ce8d58d39fd15c81f15b17c76f
Devin Power <dpwr16@gmail.com>
2023-07-06 11:34:47 -0500

    extract db loading and connection logic to lib

--- frontend/src/lib/components/ItemTable.svelte
+++ frontend/src/lib/components/ItemTable.svelte
@@ -1,23 +1,14 @@
 <script lang="ts">
 	import { Table, TableBody, TableHead, TableHeadCell } from 'flowbite-svelte';
 	import ItemTableRow from './ItemTableRow.svelte';
-	import { initDB } from '$lib/duckdb';
+	import { getConnection, getQueryResults, PARQUETS } from '$lib/duckdb';
 
-	// Setup and connect to the database
-	const load_db = async () => {
-		console.log('LOADING DB');
-		// Initialize database
-		const db = await initDB();
-		await db.registerFileURL('prod_item_search.parquet', '/prod_item_search.parquet', 4, false);
-		const conn = db.connect(); // Open a connection (promise)
-		return conn;
-	};
-
-	// Set up the db connection as an empty promise.
-	const conn_prom = load_db();
-
-	// SQL to send to DuckDB-Wasm
-	const prepare_query = (searchValue = '', selectedSpecYear = 2020, limit = 20, offset = 0) => {
+	const prepareQuery = (
+		searchValue: string,
+		selectedSpecYear: number,
+		limit: number,
+		offset: number
+	) => {
 		return `
         SELECT 
             id, 
@@ -25,7 +16,7 @@
             long_description as itemDescription,
             plan_unit_description as unit,
             contract_count as contractOccur
-        FROM parquet_scan('prod_item_search.parquet')
+        FROM parquet_scan(${PARQUETS.items})
         WHERE 
             in_spec_${selectedSpecYear} = TRUE AND
             item_number LIKE '%${searchValue}%' OR
@@ -36,25 +27,25 @@
         `;
 	};
 
-	// Send query and await results from DuckDB
-	const get_query = async (q: string) => {
-		const conn = await conn_prom;
-		const results = await conn.query(q);
-		return results;
+	const getRows = async (
+		searchValue: string,
+		selectedSpecYear: number,
+		limit: number,
+		offset: number
+	) => {
+		const conn = await getConnection();
+		const q = prepareQuery(searchValue, selectedSpecYear, limit, offset);
+		const results = await getQueryResults(conn, q);
+		return results.toArray();
 	};
 
+	export let searchValue = '';
+	export let selectedSpecYear = 2020;
+	export let limit = 10;
+	export let offset = 0;
+
+	$: rows = getRows(searchValue, selectedSpecYear, limit, offset);
 	const headers = ['View Bids', 'Item Number', 'Item Description', 'Unit', 'Contract Occurrences'];
-	let rows = new Promise(() => {});
-
-	const get_rows = async (searchValue = '', selectedSpecYear = 2020, limit = 20, offset = 0) => {
-		const q = prepare_query(searchValue, selectedSpecYear, limit, offset);
-		console.log(q);
-		const results = get_query(q);
-		const result = await results;
-		rows = result.toArray();
-	};
-
-	conn_prom.then(() => get_rows());
 </script>
 
 <Table hoverable class="mt-5">
@@ -65,7 +56,7 @@
 	</TableHead>
 	{#await rows}
 		<div />
-	{:then}
+	{:then rows}
 		<TableBody tableBodyClass="divide-y">
 			{#each rows as row}
 				<ItemTableRow {...row} />
--- frontend/src/lib/duckdb.ts
+++ frontend/src/lib/duckdb.ts
@@ -4,33 +4,58 @@ import mvp_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-mvp.worker.js?ur
 import duckdb_wasm_eh from '@duckdb/duckdb-wasm/dist/duckdb-eh.wasm?url';
 import eh_worker from '@duckdb/duckdb-wasm/dist/duckdb-browser-eh.worker.js?url';
 
-import type { AsyncDuckDB } from '@duckdb/duckdb-wasm'
+import type { AsyncDuckDB, AsyncDuckDBConnection } from '@duckdb/duckdb-wasm'
 
 let db: AsyncDuckDB | null = null;
 
-const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
-  mvp: {
-    mainModule: duckdb_wasm,
-    mainWorker: mvp_worker,
-  },
-  eh: {
-    mainModule: duckdb_wasm_eh,
-    mainWorker: eh_worker,
-  },
+const PARQUETS = {
+  items: 'prod_item_search.parquet',
+  bids: 'prod_bids.parquet',
+  itemWeightedAvgByYear: 'prod_item_weighted_avg_by_year.parquet'
+}
+
+// Setup and connect to the database
+const registerParquets = async (db: AsyncDuckDB) => {
+  for (const parquet of Object.values(PARQUETS)) {
+    await db.registerFileURL(parquet, `/${parquet}`, 4, false);
+  }
 };
-// Select a bundle based on browser checks
-const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
-// Instantiate the asynchronus version of DuckDB-wasm
-const worker = new Worker(bundle.mainWorker!);
-const logger = new duckdb.ConsoleLogger();
 
 const initDB = async () => {
   if (db) {
     return db;  // Return existing db if already initialized
   }
+
+  const MANUAL_BUNDLES: duckdb.DuckDBBundles = {
+    mvp: {
+      mainModule: duckdb_wasm,
+      mainWorker: mvp_worker,
+    },
+    eh: {
+      mainModule: duckdb_wasm_eh,
+      mainWorker: eh_worker,
+    },
+  };
+  // Select a bundle based on browser checks
+  const bundle = await duckdb.selectBundle(MANUAL_BUNDLES);
+  // Instantiate the asynchronus version of DuckDB-wasm
+  const worker = new Worker(bundle.mainWorker!);
+  const logger = new duckdb.ConsoleLogger();
+
   db = new duckdb.AsyncDuckDB(logger, worker);
   await db.instantiate(bundle.mainModule, bundle.pthreadWorker);
-  return db
+  await registerParquets(db)
+  return db;
 };
 
-export { initDB }; // so we can import this elsewhere
+const getConnection = async () => {
+  db = await initDB();
+  return db.connect();
+};
+
+const getQueryResults = async (conn: AsyncDuckDBConnection, q: string) => {
+  return await conn.query(q);
+};
+
+export { getConnection, getQueryResults, PARQUETS };
+
