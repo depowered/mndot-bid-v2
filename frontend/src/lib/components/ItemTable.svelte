54a5fe600374668e420e1b87fdce9bd6347197fd
Devin Power <dpwr16@gmail.com>
2023-07-06 15:49:00 -0500

    add item table pagination

--- frontend/src/lib/components/ItemTable.svelte
+++ frontend/src/lib/components/ItemTable.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Table, TableBody, TableHead, TableHeadCell, Spinner } from 'flowbite-svelte';
+	import { Table, TableBody, TableHead, TableHeadCell, Spinner, Pagination } from 'flowbite-svelte';
 	import ItemTableRow from './ItemTableRow.svelte';
 	import { getConnection, getQueryResults, PARQUETS } from '$lib/duckdb';
 
@@ -8,12 +8,7 @@
 	let limit = 10;
 	let offset = 0;
 
-	const prepareQuery = (
-		submittedSearchValue: string,
-		submittedSpecYear: number,
-		limit: number,
-		offset: number
-	) => {
+	const prepareSubQuery = (submittedSearchValue: string, submittedSpecYear: number) => {
 		return `
         SELECT 
             id, 
@@ -28,26 +23,42 @@
                 item_number LIKE '%${submittedSearchValue}%' OR
                 long_description LIKE '%${submittedSearchValue}%'
             )
-        ORDER BY item_number
-        LIMIT ${limit}
-        OFFSET ${offset}
-        `;
+        ORDER BY item_number`;
 	};
 
-	const getRows = async (
-		searchValue: string,
-		selectedSpecYear: number,
-		limit: number,
-		offset: number
-	) => {
+	const getRecordCount = async (subQuery: string): Promise<number> => {
 		const conn = await getConnection();
-		const q = prepareQuery(searchValue, selectedSpecYear, limit, offset);
+		const q = `SELECT COUNT(*) AS count FROM (${subQuery})`;
+		const results = await getQueryResults(conn, q);
+		return results.toArray()[0].count;
+	};
+
+	const getRows = async (subQuery: string, limit: number, offset: number) => {
+		const conn = await getConnection();
+		const q = `SELECT * FROM (${subQuery}) LIMIT ${limit} OFFSET ${offset}`;
 		const results = await getQueryResults(conn, q);
 		return results.toArray();
 	};
 
-	$: rows = getRows(submittedSearchValue, submittedSpecYear, limit, offset);
+	$: subQuery = prepareSubQuery(submittedSearchValue, submittedSpecYear);
+
+	// Table content
+	$: rows = getRows(subQuery, limit, offset);
 	const headers = ['View Bids', 'Item Number', 'Item Description', 'Unit', 'Contract Occurrences'];
+
+	// Pagination
+	// Reset the value of offset on new search to reset paging
+	const resetOffset = (submittedSearchValue: string) => 0;
+	$: offset = resetOffset(submittedSearchValue);
+
+	$: recordCount = getRecordCount(subQuery);
+	const previous = () => {
+		offset - limit >= limit ? (offset -= limit) : (offset = 0);
+	};
+	const next = async () => {
+		const max = await recordCount;
+		offset + limit < max ? (offset += limit) : (offset = offset);
+	};
 </script>
 
 {#await rows}
@@ -67,4 +78,19 @@
 			{/each}
 		</TableBody>
 	</Table>
+{/await}
+{#await recordCount then recordCount}
+	<div class="flex flex-col items-center justify-center gap-2">
+		<div class="text-sm text-gray-700 dark:text-gray-400">
+			Showing <span class="font-semibold text-gray-900 dark:text-white">{offset + 1}</span> to
+			<span class="font-semibold text-gray-900 dark:text-white"
+				>{offset + limit < recordCount ? offset + limit : recordCount}</span
+			>
+			of <span class="font-semibold text-gray-900 dark:text-white">{recordCount}</span> Entries
+		</div>
+
+		<Pagination table on:previous={previous} on:next={next}>
+			<span slot="prev">Prev</span>
+		</Pagination>
+	</div>
 {/await}
