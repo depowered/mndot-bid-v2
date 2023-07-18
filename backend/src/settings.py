df06005cfc59f089ab0a2c2267cb7fa56c8ca9c4
Devin Power <dpwr16@gmail.com>
2023-07-18 16:12:05 +0000

    fix conflict with pydantic HttpUrl and httpx.URL types

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 
-from pydantic import Field, HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 PROJECT_DIR = Path(__file__).resolve().parents[1]
@@ -19,18 +18,19 @@ class Settings(BaseSettings):
     raw_abstract_dir: Path = data_dir / "raw/abstracts/"
     split_abstract_dir: Path = data_dir / "interim/split_abstracts/"
     clean_abstract_dir: Path = data_dir / "interim/clean_abstracts/"
-    mndot_abstracts_app: HttpUrl = Field(
-        default="https://transport.dot.state.mn.us/PostLetting/Abstract.aspx"
+    mndot_abstracts_app: str = (
+        "https://transport.dot.state.mn.us/PostLetting/Abstract.aspx"
     )
-    download_abstract_url: HttpUrl = Field(
-        default="https://transport.dot.state.mn.us/PostLetting/abstractCSV.aspx"
+
+    download_abstract_url: str = (
+        "https://transport.dot.state.mn.us/PostLetting/abstractCSV.aspx"
     )
 
     # Item list processing
     raw_item_list_dir: Path = data_dir / "raw/item_lists/"
     clean_item_list_dir: Path = data_dir / "interim/clean_item_lists/"
-    download_item_list_url: HttpUrl = Field(
-        default="https://transport.dot.state.mn.us/Reference/refItem.aspx"
+    download_item_list_url: str = (
+        "https://transport.dot.state.mn.us/Reference/refItem.aspx"
     )
 
     # Data Build Tool (dbt)
