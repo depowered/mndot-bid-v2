b6fa6e84344b9e2414f7e8ca3bb3dc2d59cc6e9d
Devin Power <dpwr16@gmail.com>
2023-07-19 21:02:57 +0000

    fix update bash script

--- backend/scripts/update_prod_parquets.sh
+++ backend/scripts/update_prod_parquets.sh
@@ -1,8 +1,8 @@
-#!bin/sh
+#!/bin/bash
 
 # Runs commands to update the parquets in the prod bucket with the latest source data
-poetry run mndot-bid-cli item run-pipeline
-poetry run mndot-bid-cli abstract run-pipeline
-poetry run mndot-bid-cli dbt run
-poetry run mndot-bid-cli s3 sync-dev-to-s3
-poetry run mndot-bid-cli s3 sync-prod-to-s3
+mndot-bid-cli item run-pipeline
+mndot-bid-cli abstract run-pipeline
+mndot-bid-cli dbt run
+mndot-bid-cli s3 sync-dev-to-s3
+mndot-bid-cli s3 sync-prod-to-s3
