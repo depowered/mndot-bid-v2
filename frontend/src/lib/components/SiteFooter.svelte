5db6fe68a46af8a98a4a736f7f6df632f3f2ed91
Devin Power <dpwr16@gmail.com>
2023-07-25 16:48:11 -0500

    add basic footer with links to github and linkedin

---
+++ frontend/src/lib/components/SiteFooter.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from 'flowbite-svelte';
+</script>
+
+<Footer>
+	<FooterCopyright
+		href="https://www.linkedin.com/in/devin-power/"
+		by="Devin Power"
+		year={2023}
+		copyrightMessage=""
+	/>
+	<FooterLinkGroup
+		ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+	>
+		<FooterLink href="https://github.com/depowered/mndot-bid-v2">GitHub</FooterLink>
+		<FooterLink href="https://github.com/depowered/mndot-bid-v2/LICENSE">Licensing</FooterLink>
+	</FooterLinkGroup>
+</Footer>
--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script>
 	import '../app.postcss';
 	import Header from '../lib/components/Header.svelte';
+	import SiteFooter from '../lib/components/SiteFooter.svelte';
 </script>
 
 <Header />
-<main class="container mx-auto h-screen">
+<main class="container mx-auto">
 	<slot />
 </main>
+
+<SiteFooter />
